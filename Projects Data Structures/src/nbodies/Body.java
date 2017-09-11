bb7c66921d9491089020a993bb71f9b5f3e420d9
KosmicCo <cruzbarnum@gmail.com>
2017-09-10 20:44:26 -0700

    It works sufficiently.

--- Projects Data Structures/src/nbodies/Body.java
+++ Projects Data Structures/src/nbodies/Body.java
@@ -5,6 +5,7 @@
  */
 package nbodies;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.Vector;
 
 /**
@@ -13,27 +14,14 @@ import edu.princeton.cs.algs4.Vector;
  */
 public class Body {
     
+    private static final double THREE_HALFS = 3 / 2;
+    
     private Vector coords;
     private Vector velocity;
     private double mass;
     private String gifFile;
-    private Object gif; //This is broken 
     
-    public Body(String raw){
-        String[] split = raw.split(" ");
-        double xc = Double.parseDouble(split[0].replaceAll("\\s", ""));
-        double yc = Double.parseDouble(split[1].replaceAll("\\s", ""));
-        double xv = Double.parseDouble(split[2].replaceAll("\\s", ""));
-        double yv = Double.parseDouble(split[3].replaceAll("\\s", ""));
-        double ms = Double.parseDouble(split[4].replaceAll("\\s", ""));
-        String gf = split[5].replaceAll("\\s", "");
-        coords = new Vector(xc, yc);
-        velocity = new Vector(xv, yv);
-        mass = ms;
-        gifFile = gf;
-    }
-    
-    private Body(Vector c, Vector v, double ms, String gf){
+    public Body(Vector c, Vector v, double ms, String gf){
         coords = c;
         velocity = v;
         mass = ms;
@@ -41,7 +29,21 @@ public class Body {
     }
     
     public void update(){
-        
+        Vector netForce = new Vector(0, 0);
+        for(Body b : Sim.bodies){
+            if(b != this){
+                double px = b.getCoords().cartesian(0) - coords.cartesian(0);
+                double py = b.getCoords().cartesian(1) - coords.cartesian(1);
+                double conF = (6.67e-11 * mass * b.getMass()) / Math.pow(px * px + py * py, THREE_HALFS);
+                netForce = netForce.plus(new Vector(conF * px, conF * py));
+            }
+        }
+        velocity = velocity.plus(netForce.scale(Sim.increment / mass));
+        coords = coords.plus(velocity.scale(Sim.increment));
+    }
+    
+    public void draw(){
+        StdDraw.picture((coords.cartesian(0) + Sim.uniRadius) / Sim.uniDiameter, (coords.cartesian(1) + Sim.uniRadius) / Sim.uniDiameter, gifFile);
     }
 
     public Vector getCoords() {
--- Projects Data Structures/src/nbodies/Sim.java
+++ Projects Data Structures/src/nbodies/Sim.java
@@ -7,6 +7,7 @@ package nbodies;
 
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.Vector;
 import java.util.ArrayList;
 
 /**
@@ -20,18 +21,33 @@ public class Sim {
     public static double duration;
     public static double increment;
     public static double uniRadius;
+    public static double uniDiameter;
     public static ArrayList<Body> bodies;
     
     public static void main(String[] args) {
-        String[] gggggg = {"1.0", "1.0", "planets.txt"};
+        String[] gggggg = { "1000.0", "0.1", "atom.txt"};
         initFromFile(gggggg);
         draw();
-        
+        double curtime = 0.0;
+        double waittime = 100;
+        double milli = System.currentTimeMillis();
+        while(curtime < duration){
+            update();
+            draw();
+            curtime += Sim.increment;
+            while(System.currentTimeMillis() - milli < waittime){}
+            milli = System.currentTimeMillis();
+        }
+        System.out.println("ryui");
+    }
+    
+    public static void update(){
+        bodies.forEach(b -> b.update());
     }
     
     public static void draw(){
         StdDraw.picture(0, 0, BACK, 2, 2);
-        bodies.forEach(b -> draw());
+        bodies.forEach(b -> b.draw());
     }
     
     public static void initFromFile(String[] args){
@@ -39,16 +55,15 @@ public class Sim {
             duration = Double.parseDouble(args[0]);
             increment = Double.parseDouble(args[1]);
             In fileInput = new In("nbody/nbody/" + args[2]);
-            String[] lines = fileInput.readAllLines();
-            int planNum = Integer.parseInt(lines[0].replaceAll("\\s", ""));
-            uniRadius = Double.parseDouble(lines[1].replaceAll("\\s", ""));
+            int planNum = fileInput.readInt();
+            uniRadius = fileInput.readDouble();
+            uniDiameter = uniRadius * 2;
             bodies = new ArrayList();
+            
             for (int i = 0; i < planNum; i++) {
-                double[] info = new double[5];
-                for (int j = 0; j < 5; j++) {
-                    info[j] = fileInput.readDouble(); //thats now how it works
-                }
-                bodies.add(new Body(info, fileInput.readString()));
+                Vector pos = new Vector(fileInput.readDouble(), fileInput.readDouble());
+                Vector vel = new Vector(fileInput.readDouble(), fileInput.readDouble());
+                bodies.add(new Body(pos, vel, fileInput.readDouble(), "nbody/nbody/" + fileInput.readString()));
             }
 //        }catch(NumberFormatException nfe){
 //            System.out.println("Needs double input");
