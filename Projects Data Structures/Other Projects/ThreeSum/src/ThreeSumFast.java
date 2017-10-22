6f8705c64f9bcaebc2e4e1e229a08837a6ce1e1c
KosmicCo <cruzbarnum@gmail.com>
2017-10-21 22:42:57 -0700

    Need nKints.txt for esting of ThreeSum

--- Projects Data Structures/Other Projects/ThreeSum/src/ThreeSumFast.java
+++ Projects Data Structures/Other Projects/ThreeSum/src/ThreeSumFast.java
@@ -1,5 +1,9 @@
 
+import edu.princeton.cs.algs4.BinarySearch;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Quick;
+import edu.princeton.cs.algs4.StdRandom;
+import java.math.BigInteger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,23 +18,45 @@ import edu.princeton.cs.algs4.In;
 public class ThreeSumFast {
     
     public static String filename;
+    public static Integer[] integers;
     public static int[] ints;
+    public static int runs;
     
     //public static int count();
     
     public static void main(String[] args) {
-        if(args.length == 2){
+        if(args.length == 3){
             filename = args[0];
-            ints = new int[Integer.parseInt(args[1])];
+            integers = new Integer[Integer.parseInt(args[1])];
+            ints = new int[integers.length];
+            runs = Integer.parseInt(args[2]);
         }else{
             filename = "1Kints.txt";
-            ints = new int[1000];
+            integers = new Integer[1000];
+            ints = new int[integers.length];
+            runs = 10;
         }
         
         In fn = new In(filename);
         
-        for (int i = 0; i < ints.length; i++) {
-            ints[i] = fn.readInt();
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = fn.readInt();
         }
+        
+        BigInteger sum = new BigInteger("0");
+        Quick.sort(integers);
+        for (int i = 0; i < integers.length; i++) {
+            ints[i] = integers[i];
+        }
+        long tStart, tEnd;
+        int randIndex;
+        for (int i = 0; i < runs; i++) {
+            randIndex = StdRandom.uniform(integers.length);
+            tStart = System.nanoTime();
+            BinarySearch.indexOf(ints, randIndex);
+            tEnd = System.nanoTime();
+            sum.add(new BigInteger("" + (tEnd - tStart)));
+        }
+        System.out.println(sum.divide(new BigInteger("" + runs)).doubleValue() / 1000000000);
     }
 }
