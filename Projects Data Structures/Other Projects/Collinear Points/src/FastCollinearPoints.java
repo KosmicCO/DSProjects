178ef2d7e8672d4cd422eeba40f456efd298220b
cbarnum18 <cbarnum18@S-RM44-06.jesuitpdx.org>
2017-11-14 10:15:21 -0800

    Collinear Points

--- Projects Data Structures/Other Projects/Collinear Points/src/FastCollinearPoints.java
+++ Projects Data Structures/Other Projects/Collinear Points/src/FastCollinearPoints.java
@@ -18,9 +18,37 @@ public class FastCollinearPoints {
     private LineSegment[] segments;
     
     public FastCollinearPoints(Point[] points){
+        if(points == null){
+            throw new IllegalArgumentException();
+        }
         this.points = points;
+        
+        for (Point point : points) {
+            if (point == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+        
         Arrays.sort(points);
         
+        for (int i = 0; i < points.length - 1; i++) {
+            if(points[i].compareTo(points[i + 1]) == 0){
+                throw new IllegalArgumentException();
+            }
+        }
+        
+        Point[] pc = Arrays.copyOf(points, points.length);
+        
+        Point a;
+        for (int i = 0; i < points.length - 3; i++) {
+            
+            a = points[i];
+            Arrays.sort(pc, a.slopeOrder());
+            
+            for (int j = i + 1; j < points.length; j) {
+                
+            }
+        }
     }
     
     public int numberOfSegments(){
