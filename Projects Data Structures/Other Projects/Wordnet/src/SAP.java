c0ee20a2fe84cd9b8ca13e001ab044bc36419b19
cbarnum18 <cbarnum18@S-RM44-06.jesuitpdx.org>
2018-04-10 10:15:24 -0700

    Wordnet

--- Projects Data Structures/Other Projects/Wordnet/src/SAP.java
+++ Projects Data Structures/Other Projects/Wordnet/src/SAP.java
@@ -1,5 +1,7 @@
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Queue;
+import java.util.Arrays;
 import java.util.Iterator;
 
 /*
@@ -14,18 +16,18 @@ import java.util.Iterator;
 public class SAP {
 
     private boolean[] vChecked, wChecked;
-    private boolean[] vLength, wLength;
     private Digraph graph;
     
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph g) {
+        graph = copyDigraph(g);
         vChecked = new boolean[g.V()];
         wChecked = new boolean[g.V()];
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-
+        return length(() -> new SingleIterator(v), () -> new SingleIterator(w));
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
@@ -34,27 +36,67 @@ public class SAP {
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w) {
-
+    public synchronized int length(Iterable<Integer> v, Iterable<Integer> w) {
+        wipe(vChecked);
+        wipe(wChecked);
+        Queue<Integer> vQueue = new Queue();
+        Queue<Integer> wQueue = new Queue();
+        
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        
+    public synchronized int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        wipe(vChecked);
+        wipe(wChecked);
+        Queue<Integer> bfsQueue = new Queue();
+        for(int i : v){
+            bfsQueue.enqueue(i);
+            vChecked[i] = true;
+        }
+        for(int i : w){
+            if(vChecked[i])return i;
+            bfsQueue.enqueue(-(i + 1));
+            wChecked[i] = true;
+        }
+        int cur;
+        while(!bfsQueue.isEmpty()){
+            cur = bfsQueue.dequeue();
+            if(cur < 0){
+                cur = -(cur + 1);
+                for(int i : graph.adj(cur)){
+                    if(wChecked[i]) continue;
+                    if(vChecked[i]) return i;
+                    bfsQueue.enqueue(-(i + 1));
+                    wChecked[i] = true;
+                }
+            }else{
+                for(int i : graph.adj(cur)){
+                    if(vChecked[i]) continue;
+                    if(wChecked[i]) return i;
+                    bfsQueue.enqueue(i);
+                    vChecked[i] = true;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    private static void wipe(boolean[] array){
+        Arrays.fill(array, 0, 0, false);
+    }
+    
+    private static Digraph copyDigraph(Digraph g){
+        Digraph copy = new Digraph(g.V());
+        int len = g.V();
+        for (int i = 0; i < len; ++i) {
+            for(int j : g.adj(i)) copy.addEdge(i, j);
+        }
+        return copy;
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
 
-    }
-    
-    private class CommonAncestor{
-        
-        private int length, ancestor;
-        
-        public CommonAncestor(int l, int a){
-            
-        }
     }
 
     private class SingleIterator implements Iterator<Integer> {
