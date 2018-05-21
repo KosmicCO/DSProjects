7a72914594ddaaa10c8e660d4a55f50dd322bf02
cbarnum18 <cbarnum18@S-RM44-06.jesuitpdx.org>
2018-05-21 09:35:44 -0700

    CTF

---
+++ Projects Data Structures/Other Projects/CTF/src/Fermat.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cbarnum18
+ */
+public class Fermat {
+    
+    public static void main(String[] args) {
+        int c = 3504;
+        System.out.println(c(c));
+        int a = 1;
+        int b = c(c) - c(a);
+        double root = Math.pow(b, 0.33333333333333333333333333333333333333);
+        while(b <= 0 || Math.abs(root - ((int) root)) > 1e-10){
+            if(b < 0){
+                c++;
+                a = 1;
+            }else{
+                a++;
+            }
+            b = c(c) - c(a);
+            root = Math.pow(b, 0.33333333333333333333333333333333333333);
+        }
+        System.out.println(a + ", " + b + ", " + c);
+    }
+    
+    public static int c(int i){
+        return i * i * i;
+    }
+}
